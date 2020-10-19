f08ae8ec1b8cca05501fd79bc6fc62a05e317386
Hongze Xia <hongze.x@gmail.com>
2020-10-19 21:59:52 +1100

    add styleguide, router-dom

---
+++ src/components/Table.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Table() {
+    return <table>
+        <th>params.abs</th>
+        <tr>
+            <td>1</td>
+        </tr>
+    </table>
+}
+
+export default Table;
