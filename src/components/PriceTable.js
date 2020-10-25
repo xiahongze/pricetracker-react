80725d6d117e2b47e6c67a0bd05dad23f277a567
Hongze Xia <hongze.x@gmail.com>
2020-10-25 22:03:43 +1100

    fixup pricetable warning

--- src/components/PriceTable.js
+++ src/components/PriceTable.js
@@ -16,26 +16,30 @@ function PriceTable(props) {
     const [isLoaded, setIsLoaded] = useState(false);
     const [items, setItems] = useState([]);
     const [page, setPage] = useState({});
-    const updateFunc = async () => {
-        try {
-            // promise chaining, make sure that list of promises are converted
-            // to a single promise with Promise.all
-            const [page, prices] = await Promise.all([
-                utils.get(config.pageApi, { idx: pageId }),
-                utils.get(config.priceApi, { page_id: pageId })
-            ]).then(resps => Promise.all(resps.map(resp => resp.json())));
-            setPage(page);
-            setItems(prices);
-            setIsLoaded(true);
-        } catch (error) {
-            setIsLoaded(true);
-            setError(error);
-        }
-    };
+
 
     useEffect(() => {
+        // the async function is better to be defined within the local scope such
+        // that it is not affecting the rendering of the view as it may change
+        // unintentionally because of the states it captures.
+        const updateFunc = async () => {
+            try {
+                // promise chaining, make sure that list of promises are converted
+                // to a single promise with Promise.all
+                const [page, prices] = await Promise.all([
+                    utils.get(config.pageApi, { idx: pageId }),
+                    utils.get(config.priceApi, { page_id: pageId })
+                ]).then(resps => Promise.all(resps.map(resp => resp.json())));
+                setPage(page);
+                setItems(prices);
+                setIsLoaded(true);
+            } catch (error) {
+                setIsLoaded(true);
+                setError(error);
+            }
+        };
         updateFunc();
-    }, []); // the deps is an empty list as we only need to load it once
+    }, [pageId]); // the deps are only about the things it needs
 
     if (error) {
         return <Container>Error: {error.message}</Container>;
